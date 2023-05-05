a0bcd1394fc743c7a2e5f2a8e99a297974c6e314
Wenzgate <cornelis.m@outlook.com>
2023-05-06 00:41:39 +0200

    Add full drag and drop

--- script/app.js
+++ script/app.js
@@ -1,64 +1,39 @@
+"use strict"
 
+// Loading Bar //
  
- function startProgressBar() {
-   let percent = 0;
-   const sectionLoading = document.querySelector('.section_loading')
-   const progress = document.querySelector(".progress__bar");
-   const percentText = document.querySelector(".progress__percent");
+function startProgressBar() {
+  let percent = 0;
+  const sectionLoading = document.querySelector('.section_loading')
+  const progress = document.querySelector(".progress__bar");
+  const percentText = document.querySelector(".progress__percent");
 
-   const intervalId = setInterval(function() {
+  const intervalId = setInterval(function() {
      // Randomize progress
-     percent += Math.floor(Math.random() * 35);
-     percent = percent > 98 ? 98 : percent;
-
+    percent += Math.floor(Math.random() * 35);
+    percent = percent > 98 ? 98 : percent;
      // Update progress bar
-     progress.style.width = percent + "%";
-     percentText.textContent = percent + "%";
-
+    progress.style.width = percent + "%";
+    percentText.textContent = percent + "%";
      // Finish if progress is 100%
-     if (percent >= 98) {
-       clearInterval(intervalId);
-       gsap.to(sectionLoading, { 
-         x: "-100%", 
-         duration: 1, 
-         delay: .5, 
-         ease:"power4.inOut"  
-     });
+    if (percent >= 98) {
+      clearInterval(intervalId);
+      gsap.to(sectionLoading, { 
+        x: "-100%", 
+        duration: 1, 
+        delay: .5, 
+        ease:"power4.inOut"  
+      });
+    }
+  }, 500);
+}
 
-     }
-   }, 500);
- }
 startProgressBar();
 
-// const elements = document.querySelectorAll('.gridSelection__item')
-// let currentElement = null;
+// Loading Bar //
 
-// elements.forEach(element => {
-//   element.addEventListener('click', () => {
 
-//     var sourceStyle = window.getComputedStyle(element);
-//     var sourceBackground = sourceStyle.getPropertyValue('background-image');
-//     var target = document.querySelector('.section__customize__avatar');
-
-//     target.style.backgroundImage = sourceBackground;
-//     // si un élément est déjà agrandi, le redimensionne à sa taille initiale
-//     if (currentElement) {
-//       gsap.to(currentElement, { scale: 1,duration:0.1, ease:"power1.in" });
-//     }
-
-//     // si l'élément actuel n'est pas celui qui vient d'être cliqué, l'agrandit
-//     if (currentElement !== element) {
-//         gsap.timeline()
-//         .to(element, {scale:.9, duration:0.1, ease:"power1.in"})
-//         .to(element, {scale:1.1,duration: 0.2, ease:"power4.inOut" }) // déplacement de l'avatar au centre de l'écran
-//         .play(); // lancement de l'animation
-//       currentElement = element;
-//     } else {
-//       currentElement = null;
-//     }
-//   });
-// });
-
+// Sliders //
 
 const sliders = document.querySelectorAll('input[type="range"]');
 const values = document.querySelectorAll('label[class$="--value"]');
@@ -74,36 +49,33 @@ sliders.forEach((slider, index) => {
 });
 
 
-// var next = document.querySelector('.section__selection__btn')
-// var sectionSelection = document.querySelector('.section_selection')
+// Sliders //
 
-// next.addEventListener('click', () =>{
-//   gsap.to(sectionSelection, { 
-//     x: "-100%", 
-//     duration: 1, 
-//     delay: .5, 
-//     ease:"power4.inOut"  
-// })
-// })
+
+// Avatar Selection //
+
 
 const gridItems = document.querySelectorAll('.gridSelection__item');
 const body = document.querySelector('body');
 const containerCustomize = document.querySelector('.container__customize')
 const sectionSelection = document.querySelector('.section__selection')
-
-
+const drag_item = document.querySelector('.draggable') 
 
 const centerX = body.clientWidth / 2 - 75;
 const centerY = body.clientHeight / 2 - 300;
 
 gridItems.forEach(item => {
   item.addEventListener('click', () => {
+
+    item.classList.add('dropzone')
+
     gsap.to(item, {
       left: centerX,
       top: centerY,
       scale: 2,
       duration: .5,
       position : "absolute",
+      pointerEvents :'none',
       ease : "power4.inOut",
       
     });
@@ -113,6 +85,7 @@ gridItems.forEach(item => {
       if (otherItem !== item) {
         gsap.to(otherItem, {
           opacity:0,
+          display:'none',
           ease : "power4.inOut",
           onComplete : showSlider
         });
@@ -120,64 +93,152 @@ gridItems.forEach(item => {
 
     });
 
-        function showSlider(){
-          gsap.set(containerCustomize, {
-            display:"flex"
-          });
-        }
-
-    
+    function showSlider(){
+      gsap.set(containerCustomize, {
+        display:"flex"
+      });
+    }
   });
 });
 
 
+// Avatar Selection //
 
-// Sélectionnez l'élément à glisser
-const draggableElement = document.querySelector('.draggable');
 
-// Enregistrer la position initiale de l'élément
+// Dragging //
+
+
 let initialX, initialY;
-interact(draggableElement)
-  .draggable({
-    // activer la détection des collisions avec d'autres éléments
-    autoScroll: true,
-    // définir une fonction pour restreindre le déplacement dans un parent
-    restrict: {
-      restriction: document.querySelector('.container'),
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-      endOnly: true
-    },
-    // appelé lorsqu'un élément est déplacé
-    onmove: function (event) {
-      // calculer la nouvelle position en ajoutant le mouvement à la position initiale
-      const target = event.target;
-      const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-      const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-      // déplacer l'élément à la nouvelle position
-      target.style.transform = `translate(${x}px, ${y}px)`;
+interact('.draggable').draggable({
+  autoScroll: true,
+  restrict: {
+    restriction: document.querySelector('.container'),
+    elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+    endOnly: true
+  },
 
-      // mettre à jour les attributs de position de l'élément
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-    },
-    // appelé lorsqu'un élément est activé pour le glissement
-    onstart: function (event) {
-      // enregistrer la position initiale de l'élément
-      initialX = parseFloat(event.target.getAttribute('data-x')) || 0;
-      initialY = parseFloat(event.target.getAttribute('data-y')) || 0;
-    },
-    // appelé lorsqu'un élément est relâché après un glissement
-    onend: function (event) {
-      // réinitialiser la position de l'élément à la position initiale
-      //event.target.style.transform = `translate(${initialX}px, ${initialY}px)`;
-      gsap.to(event.target, {
-        x: initialX ,
-        y: initialY ,
-        ease: "power4.inOut",
-        duration:.5
+  onmove: function (event) {
+    const target = event.target;
+    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    target.style.transform = `translate(${x}px, ${y}px)`;
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  },
+
+  onstart: function (event) {
+    initialX = 0
+    initialY = 0;
+  },
+
+  onend: function (event) {
+    const target = event.target;
+    
+    if (target.hasAttribute('data-dropped')) {
+      // Make the element disappear
+      gsap.to(target, {
+        opacity: 0,
+        duration: 0.3,
+        onComplete: function () {
+          // Reset the element's position and make it reappear
+          target.style.transform = `translate(${initialX}px, ${initialY}px)`;
+          target.setAttribute('data-x', initialX);
+          target.setAttribute('data-y', initialY);
+          gsap.set(target,{
+            width : targetWidth / 1.5,
+            height : targetHeight / 1.5,
+            onComplete : function () {
+              gsap.to(target, { opacity: 1 });
+            }
+          })
+
+          
+          
+          // Remove the data-dropped attribute
+          target.removeAttribute('data-dropped');
+        },
       });
+    } else {
+      // Return the element to its original position
+      target.classList.add('return-animation');
+      target.style.transform = `translate(${initialX}px, ${initialY}px)`;
+    }
+
+    event.target.addEventListener('transitionend', () => {
+
+      event.target.classList.remove('return-animation');
       event.target.setAttribute('data-x', initialX);
       event.target.setAttribute('data-y', initialY);
-    }
-  });
+    })
+        
+  },
+  
+});
+
+
+
+const targetWidth = drag_item.offsetWidth * 1.5
+const targetHeight = drag_item.offsetHeight * 1.5
+
+
+// Dragging //
+
+
+// Dropping //
+
+interact('.dropzone').dropzone({
+
+  accept: '#yes-drop',
+  overlap: 0.75,
+
+  ondropactivate: function (event) {
+    event.target.classList.add('drop-active')
+  },
+
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget
+    var dropzoneElement = event.target
+
+    dropzoneElement.classList.add('drop-target')
+    draggableElement.classList.add('can-drop')
+
+    gsap.to(draggableElement,{
+      width : targetWidth,
+      height : targetHeight,
+      duration : .1,
+      ease : "power1.in"
+    })
+  },
+
+  ondragleave: function (event) {
+    var draggableElement = event.relatedTarget
+    event.target.classList.remove('drop-target')
+    event.relatedTarget.classList.remove('can-drop')
+    
+    gsap.to(draggableElement,{
+      width : targetWidth / 1.5,
+      height : targetHeight / 1.5,
+      duration : .1,
+      ease : "power1.in"
+    })
+  },
+
+  ondrop: function (event) {
+    var draggableElement = event.relatedTarget;
+    
+    // Add a custom attribute to indicate that the element has been dropped
+    draggableElement.setAttribute('data-dropped', 'true');
+  },
+  
+  ondropdeactivate: function (event) {
+    event.target.classList.remove('drop-active')
+    event.target.classList.remove('drop-target')
+  }
+})
+
+  
+// Dropping //
+
+  
